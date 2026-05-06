2902b80f94338739a12f2794dcc46df4b42ccf65
Emre Uzun <uzun35208@gmail.com>
2026-05-06 23:19:41 +0300

    added one day report one month report tree month

--- app/Http/Controllers/Api/AppointmentController.php
+++ app/Http/Controllers/Api/AppointmentController.php
@@ -35,7 +35,7 @@ class AppointmentController extends Controller
 
     public function show(Studio $studio, Appointment $appointment): JsonResponse
     {
-        return response()->json($appointment);
+        return response()->json($studio);
         abort_if($appointment->studio_id !== $studio->id, 404);
 
         $appointment->load(['createdBy', 'assignedDriver']);
